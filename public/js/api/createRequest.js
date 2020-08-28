43b060c47c76494f893143683f8226207ac4ac41
zarajskysam <zarajskysam92@gmail.com>
2020-08-28 14:09:56 +0300

    commit added

---
+++ public/js/api/createRequest.js
@@ -0,0 +1,62 @@
+/**
+ * Основная функция для совершения запросов
+ * на сервер.
+ * */
+const createRequest = (options = {}) => {
+    let resultXhr;
+    let resultError;
+    if (options.method === 'GET') {
+        const xhr = new XMLHttpRequest();
+        xhr.open( 'GET', options.url + '?mail=' + options.data.mail + '&password=' + options.data.password );
+        try {
+            xhr.send();
+            xhr.withCredentials = true;
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    resultXhr = xhr.responseText;
+                    resultError = null; 
+                    options.callback = (resultXhr, resultError)=>{ 
+                        console.log(resultError);
+                        console.log(resultXhr);
+                    }
+                }
+            }
+        }
+        catch(err) {
+            resultError = err;
+            resultXhr = null;
+            options.callback = (resultXhr, resultError)=>{
+                console.log(resultError);
+            };
+        }
+    } else {
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+
+        formData.append( 'mail', options.data.mail );
+        formData.append( 'password', options.data.password );
+
+        xhr.open( options.method , options.url);
+        try {
+            xhr.send( formData );
+            xhr.withCredentials = true;
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    resultXhr = xhr.responseText;
+                    resultError = null;
+                    options.callback = (resultXhr, resultError)=>{
+                        console.log(resultError);
+                        console.log(resultXhr);
+                    }
+                }
+            }
+        } catch (err) {
+            resultError = err;
+            resultXhr = null;
+            options.callback = (resultXhr, resultError)=>{
+                console.log(resultError);
+            };
+        }
+       
+    }
+}
